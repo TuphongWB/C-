b68a09068cfce9f786b5197cc36b584b2489e733
Acer <Tuphong2611@gmail.com>
2023-05-15 00:20:44 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program{
+    public static void Main(string[] args)
+    {
+        DateTime localDate = DateTime.Now;
+        System.Console.WriteLine("Datetime Now is :" + localDate);
+        Xinchao();
+    }
+    static void Xinchao(){
+        Console.WriteLine("Tôi là superhero");
+    }
+}
